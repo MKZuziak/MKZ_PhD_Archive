e7938f6b5f9c3c36e8d105c64a5bf2f4a6969b87
Scolpe <63779111+Scolpe@users.noreply.github.com>
2024-10-17 15:35:39 +0200

    Materials for Chapter 5

---
+++ Chapter_5/Numerical_Results/V/simulations/fmnist/hom/script.py
@@ -0,0 +1,53 @@
+"""This example envisages traning a common Convolutional Neural Network on a number of distributed datasets using
+classical FedOpt algorithm. Local datasets are uniformly distributed, but some of them are transformed (noised) 
+according to predefined schema. During the training, three contribution metrics are defined: LOO, LSAA and EXLSAA."""
+
+import pickle
+import os
+import datasets
+from forcha.components.orchestrator.evaluator_orchestrator import Evaluator_Orchestrator
+from forcha.components.settings.evaluator_settings import EvaluatorSettings
+from model import create_FashionMnistNet
+
+def simulation():
+    cwd = os.getcwd()
+    settings = EvaluatorSettings(
+        simulation_seed=121,
+        global_epochs=80,
+        local_epochs=3,
+        number_of_nodes=10,
+        sample_size=10,
+        optimizer='SGD',
+        batch_size=32,
+        learning_rate=0.01,
+        alpha_sample=True,
+        loo_sample=True,
+        line_search_length=5,
+        parallelization = False,
+        save_central_model=False,
+        save_nodes_models=False
+        )
+    
+    with open(f'/home/maciejzuziak/Uniform_Contribution_Paper-1/PhD_experiments/V/datasets/fmnist/hom/FMNIST_10_dataset_pointers', 'rb') as file:
+        data = pickle.load(file)
+    # DATA: Selecting data for the orchestrator
+    orchestrator_data = data[0]
+    # DATA: Selecting data for nodes
+    nodes_data = data[1]
+    #nodes_data = data[1]
+    model = create_FashionMnistNet()
+    orchestrator = Evaluator_Orchestrator(
+        settings=settings, 
+        full_debug=True,
+        parallelization=False)
+    
+    
+    orchestrator.prepare_orchestrator(
+         model=model, 
+         validation_data=orchestrator_data)
+    orchestrator.prepare_training(nodes_data=nodes_data)
+    signal = orchestrator.train_protocol()
+    return signal
+
+if __name__ == "__main__":
+       simulation()
